2e52f0196a65639b429bc93797d1147379f83718
matt <emal>
2023-01-17 15:20:56 -0600

    Wk2 Lab2, Input in #2 not reading correctly

--- no_duplicates.py
+++ no_duplicates.py
@@ -8,20 +8,33 @@ class Author:
         self.books = []
 
     def publish(self, title):
-        self.books.append(title)
+       # Add duplication check
+        if title not in self.books:
+            self.books.append(title)
+        else:
+            print(f'{title} is in the list already. Please reenter a book title.')
+# TODO 
 
     def __str__(self):
         titles = ', '.join(self.books) or 'No published books'
         return f'{self.name}. Books: {titles}'
 
 def main():
+    # author_input = input('Enter an author: ')
+
+    # author = Author(author_input)
+    # author.publish = input(f'Enter a name of the book for {author_input}: ')
+    """ Using user input works but still displays NO PUBLISHED BOOKS """
+    
     tolkien = Author('J.R.R. Tolkien')
-    tolkien.publish('The Hobbit')
     tolkien.publish('The Fellowship of the Rings')
     tolkien.publish('The Two Towers')
     tolkien.publish('The Return of the King')
-
+    tolkien.publish('The Hobbit')
+    tolkien.publish('The Hobbit')
+    
     print(tolkien)
+    # print(author)
 
 main()
 print('End of program, Thank you')
--- students_gpa.py
+++ students_gpa.py
@@ -7,24 +7,29 @@ from dataclasses import dataclass
 @dataclass
 class Student:
     # Open string variable
-    name: str
+    student_name: str
     # Open integer variable
-    college_id: int
+    student_college_id: int
     # Float variable
-    gpa: float
+    student_gpa: float
 
 # Function to concatinate variables
     def __str__(self):
         # Returns concatination of variables as message
-        return f'Name {self.name}, ID: {self.college_id}, GPA: {self.gpa}'          
+        return f'Name {self.student_name}, ID: {self.student_college_id}, GPA: {self.student_gpa}'          
 
 def main():
-    # TODO Add input to get variables from user
-    alice = Student('Alice', 12345, 4.0)
-    bob = Student('Bob', 98765, 3.2)
+    # Add input to get variables from user
+    student_name = input('Enter students name: ')
+    student_college_id = input(f'Enter {student_name}\'s Student ID: ')
+    student_gpa = float(input(f'Enter {student_name}\'s GPA: ')) 
 
-# Printing the concatinated String message for each student
-    print(alice)
-    print(bob)
+
+# Renaming Variables to (s), calling on Student class to assign variables to open 
+    s = Student(student_name, student_college_id, student_gpa)
+
+    # Utilizes def __str__(self) for printing return string statement
+    print(s)
 
 main()                      # Will not display/run without this line
+print('End of program, Thank you')
